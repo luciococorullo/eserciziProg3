2fd3ca11e72543565d62b96c1b0dfff8c7c0b8db
thatslucio <luciococorullo17@gmail.com>
2020-12-11 17:11:25 +0100

    aggiustamenti

--- Thread/ExThread04.java
+++ Thread/ExThread04.java
@@ -1,28 +1,27 @@
-public class ExThread04 {
+package Thread;
 
+public class ExThread04 {
 
     public ExThread04(String[] args) {
         Runnable runnable = () -> {
-            for (int i=0;i<60;i++) {
-                System.out.println(Thread.currentThread().getName()+":"+i);
+            for (int i = 0; i < 60; i++) {
+                System.out.println(Thread.currentThread().getName() + ":" + i);
                 try {
-                    Thread.sleep((int)(Math.random()* 1000));
+                    Thread.sleep((int) (Math.random() * 1000));
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
         };
 
-        for (int i=0;i< 100; i++) {
+        for (int i = 0; i < 100; i++) {
             new Thread(runnable).start();
         }
-        System.out.println(Thread.currentThread().getName()+":--------------------------->Ended");
+        System.out.println(Thread.currentThread().getName() + ":--------------------------->Ended");
     }
 
     public static void main(String[] args) {
         new ExThread04(args);
     }
 
-
 }
-
--- Thread/ExThread05.java
+++ Thread/ExThread05.java
@@ -1,26 +1,27 @@
+package Thread;
+
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 public class ExThread05 {
 
-
     public ExThread05(String[] args) {
 
         Runnable runnable = () -> {
-            for (int i=0;i<60;i++) {
-                System.out.println(Thread.currentThread().getName()+":"+i);
+            for (int i = 0; i < 60; i++) {
+                System.out.println(Thread.currentThread().getName() + ":" + i);
                 try {
-                    Thread.sleep((int)(Math.random()* 1000));
+                    Thread.sleep((int) (Math.random() * 1000));
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
         };
 
-        //ExecutorService executorService = Executors.newSingleThreadExecutor();
+        // ExecutorService executorService = Executors.newSingleThreadExecutor();
         ExecutorService executorService = Executors.newFixedThreadPool(50);
-        for (int i=0;i< 100; i++) {
+        for (int i = 0; i < 100; i++) {
             executorService.submit(runnable);
         }
 
@@ -33,13 +34,11 @@ public class ExThread05 {
         } finally {
             executorService.shutdownNow();
         }
-        System.out.println(Thread.currentThread().getName()+":--------------------------->Ended");
+        System.out.println(Thread.currentThread().getName() + ":--------------------------->Ended");
     }
 
     public static void main(String[] args) {
         new ExThread05(args);
     }
 
-
 }
-
